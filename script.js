cfc013bfd45eab4d6539cf5d17948f1355c23bd1
Iasmym <iasmympatricia7@gmail.com>
2025-01-28 20:06:33 -0300

    declarando funçao

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("B7")
+// funçoes = algo a ser feito
+//fumçoes = oque deve ser executado
+
+// declarando variavel = 
+var nome = "Iasmym"
+
+//abrir e fechar colchetes significa inicio e fim de codigo
+// bloco de codigo{}
+//declarando funçao = 
+function exibirNome(){
+    //executado
+    console.log(nome)
+
+}
+//chamando funçao
+exibirNome();
+
+//declarando funçao
+function andar(){
+    console.log("andando");
+
+}
+
+// chamando a funçao
+andar();
